356aa4cad7c89d0dc6f528c6411cc3096ba4ffe1
HuiwooPark <parkhuiwo0@gmail.com>
2020-07-31 16:57:41 +0900

    init project

---
+++ src/main/java/io/github/parkhuiwo0/chattingDemo/ChattingDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.parkhuiwo0.chattingDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/parkhuiwo0/chattingDemo/ChattingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.parkhuiwo0.chattingDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
