4bec06f0602378bdbe4247fb45da5b581665fcc9
HuiwooPark <parkhuiwo0@gmail.com>
2020-08-01 15:17:54 +0900

    Reactive MongoDB 연동

--- src/main/java/io/github/parkhuiwo0/chattingDemo/ChattingDemoApplication.java
+++ src/main/java/io/github/parkhuiwo0/chattingDemo/ChattingDemoApplication.java
@@ -6,8 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChattingDemoApplication {
 
+//	@Autowired private MongoTemplate mongoTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChattingDemoApplication.class, args);
 	}
+
+//	@Bean
+//	public ApplicationRunner applicationRunner() {
+//		return args -> {
+//			ChattingDoc chattingDoc = new ChattingDoc();
+//
+//			chattingDoc.setContents("asdasfd");
+//			mongoTemplate.insert(chattingDoc);
+//		};
+//	}
 
 }
